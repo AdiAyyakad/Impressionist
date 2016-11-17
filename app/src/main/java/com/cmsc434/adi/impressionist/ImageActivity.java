26ad6da0703bcb7a0946a8a1ead573f784429bab
Aditya Ayyakad <njadtiya@gmail.com>
2016-11-17 09:47:01 -0500

    Initial Commit

---
+++ app/src/main/java/com/cmsc434/adi/impressionist/ImageActivity.java
@@ -0,0 +1,13 @@
+package com.cmsc434.adi.impressionist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+    }
+}
